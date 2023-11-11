a1c226ad9af611cbae27f3ea0049f985af9fbf7d
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-11-11 12:03:00 +0330

    revalidate fetched userse

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,13 +1,30 @@
+// define the type of each user 
+interface User {
+    id: number;
+    name: string;
+}
+
 async function UsersPage() {
     // fetch users
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const users = await response.json()
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+        next: { revalidate: 10 }
+    });
+    const users: User[] = await response.json()
+
     // ui
     return (
-        <div>
-            shut the fuck off
-        </div>
+        <>
+            <h1>Users</h1>
+            <br />
+            <ul>
+                {
+                    users.map((user: User) => <li key={user.id}>{user.name}</li>)
+                }
+            </ul>
+        </>
     )
 }
 
 export default UsersPage
+
+
