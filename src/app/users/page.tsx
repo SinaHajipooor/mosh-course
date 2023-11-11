1d4bbfaf6396b9fbdca8c767ffbb5500b65fdaac
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-11-11 11:27:04 +0330

    fetch users in users page

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,4 +1,8 @@
-function UsersPage() {
+async function UsersPage() {
+    // fetch users
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await response.json()
+    // ui
     return (
         <div>
             shut the fuck off
